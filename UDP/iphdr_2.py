f164f66b62ceae34846fa549c1171be805161440
seunga <osa0215@naver.com>
2022-04-20 20:43:18 +0900

    UDP

---
+++ UDP/iphdr_2.py
@@ -0,0 +1,50 @@
+from socket import *
+import struct
+import binascii
+
+class Iphdr:
+    def __init__(self, tot_len, protocol, saddr, daddr):
+        self.ver_len = 0x45
+        self.tos = 0
+        self.tos_len = tot_len
+        self.id = 0
+        self.frag_off = 0
+        self.ttl = 127
+        self.protocol = protocol
+        self.check = 0
+        self.saddr = inet_aton(saddr)
+        self.daddr = inet_aton(daddr)
+    
+    def pack_Iphdr(self):
+        packed = b''
+        packed += struct.pack('!BBH', self.ver_len, self.tos, self.tos_len)
+        packed += struct.pack('!HH', self.id, self.frag_off)
+        packed += struct.pack('!BBH', self.ttl, self.protocol, self.check)
+        packed += struct.pack('!4s', self.saddr)
+        packed += struct.pack('!4s', self.daddr)
+        return packed
+
+def unpack_Iphdr(buffer):
+    unpacked = struct.unpack('!BBHHHBBH4s4s', buffer[:20])
+    return unpacked
+
+def getPacketSize(unpacked_ipheader):
+    return unpacked_ipheader[2]
+
+def getProtocolID(unpacked_ipheader):
+    return unpacked_ipheader[6]
+
+def getIP(unpakced_ipheader):
+    src_ip = inet_ntoa(unpacked_iphdr[8])
+    dst_ip = inet_ntoa(unpacked_iphdr[9])
+    return (src_ip, dst_ip)
+
+ip = Iphdr(1000, 6, '10.0.0.1', '11.0.0.1')
+packed_iphdr = ip.pack_Iphdr()
+print(binascii.b2a_hex(packed_iphdr))
+
+unpacked_iphdr = unpack_Iphdr(packed_iphdr)
+print(unpacked_iphdr)
+
+print('Packet size:{} Protocol:{} IP:{}'.format(getPacketSize(unpacked_iphdr), getPacketSize(unpacked_iphdr), getIP(unpacked_iphdr)))
+
